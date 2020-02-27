b9864f08ae1dd05d8b98b6a4b150640e59f45629
takatronix <takatronix@gmail.com>
2020-02-27 12:26:17 +0900

    regionテーブルに所有者を追加

--- src/main/sql/region.sql
+++ src/main/sql/region.sql
@@ -1,26 +1,30 @@
-create table man10builder.region
+-- auto-generated definition
+create table region
 (
-    id       int auto_increment
+    id         int auto_increment
         primary key,
-    server   varchar(16)  not null comment 'サーバー名',
-    world    varchar(16)  not null comment 'ワールド名',
-    x        double       not null comment 'テレポートすると飛ぶ場所',
-    y        double       not null comment 'テレポートすると飛ぶ場所',
-    z        double       not null comment 'テレポートすると飛ぶ場所',
-    pitch    double       not null comment 'テレポートすると飛ぶ場所',
-    yaw      double       not null comment 'テレポートすると飛ぶ場所',
-    sx       double       null comment '始点',
-    sy       double       null comment '始点',
-    sz       double       null comment '始点',
-    ex       double       null comment '終点',
-    ey       double       null comment '終点',
-    ez       double       null comment '終点',
-    enabled  tinyint(1)   not null comment '有効か無効かフラグ
+    server     varchar(16)  not null comment 'サーバー名',
+    world      varchar(16)  not null comment 'ワールド名',
+    owner_uuid varchar(36)  null comment '所有者のUUID
+nullならだれも所有していない',
+    owner_name varchar(16)  null comment '所有者の名前',
+    x          double       not null comment 'テレポートすると飛ぶ場所',
+    y          double       not null comment 'テレポートすると飛ぶ場所',
+    z          double       not null comment 'テレポートすると飛ぶ場所',
+    pitch      double       not null comment 'テレポートすると飛ぶ場所',
+    yaw        double       not null comment 'テレポートすると飛ぶ場所',
+    sx         double       null comment '始点',
+    sy         double       null comment '始点',
+    sz         double       null comment '始点',
+    ex         double       null comment '終点',
+    ey         double       null comment '終点',
+    ez         double       null comment '終点',
+    enabled    tinyint(1)   not null comment '有効か無効かフラグ
 無効になっている場合、評価などは一切できない',
-    protect  tinyint(1)   null comment '保護フラグ
+    protect    tinyint(1)   null comment '保護フラグ
 保護がかかっている場合、regeon_authテーブルに定義されている権限者もしくはred.man10.region.auth権限をもっているもの以外はブロック破壊できない',
-    name     varchar(128) null comment '名称',
-    datetime datetime     null comment '作成日'
+    name       varchar(128) null comment '名称',
+    datetime   datetime     null comment '作成日'
 )
     comment '領域管理のためのテーブル';
 
