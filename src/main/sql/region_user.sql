b356ee481fbef48c6cfdb73b5f2af52084f5a2d2
forest611 <forest611328@gmail.com>
2020-04-05 03:01:58 +0900

    region_user.sql　テーブルの修正

--- src/main/sql/region_user.sql
+++ src/main/sql/region_user.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `region_user` (
 
+  `id` int NOT NULL auto_increment primary key,
   `region_id` int NOT NULL DEFAULT '0' COMMENT 'リージョンID',
   `type` int NOT NULL DEFAULT '0' COMMENT '0:共同所有者 1:使用者',
   `player` varchar(16) DEFAULT NULL,
@@ -9,7 +10,8 @@ CREATE TABLE `region_user` (
   `isRent` TINYINT(1) DEFAULT '0' COMMENT '1:賃貸 0:賃貸じゃない',
   `deposit` double NOT NULL DEFAULT '0' COMMENT '支払った額',
   `paid_date` datetime NOT NULL DEFAULT now() COMMENT '最後に支払った日',
-  PRIMARY KEY (`region_id`),
+  INDEX `uuid` (`uuid`),
+  INDEX `region_id` (`region_id`),
   KEY `uuid` (`uuid`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
