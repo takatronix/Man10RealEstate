d99bd62a468bb2a1e6b3742c3e71ef7a420f48b3
forest611 <forest611328@gmail.com>
2020-08-24 14:45:32 +0900

    税額通知メールを送るコマンド追加

--- src/main/kotlin/red/man10/realestate/Command.kt
+++ src/main/kotlin/red/man10/realestate/Command.kt
@@ -699,28 +699,23 @@ class Command:CommandExecutor {
 
                     return true
                 }
-//
-//                "test" ->{
-//
-//                    for (rg in region.map()){
-//                        val uuid = rg.value.ownerUUID?:continue
-//
-//                        val tax = city.getTax(city.where(rg.value.teleport),rg.key)
-//                        if (tax == 0.0)continue
-//
-//                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
-//                                "mmail send-tag Man10RealEstate ${Bukkit.getOfflinePlayer(uuid).name} &4&l[重要]土地の税金について 5 " +
-//                                        "&6&l税額:$tax;" +
-//                                        "&e&l土地ID:${rg.key};;" +
-//                                        "&6&e来月お支払いお願いします")
-//                    }
-//
-//                    plugin.logger.info("tax")
-//                    for (rg in region.map()){
-//                        val uuid = rg.value.ownerUUID?:continue
-//                        city.payingTax(uuid,rg.key)
-//                    }
-//                }
+
+                "sendtaxmail" ->{
+
+                    for (rg in region.map()){
+                        val uuid = rg.value.ownerUUID?:continue
+
+                        val tax = city.getTax(city.where(rg.value.teleport),rg.key)
+                        if (tax == 0.0)continue
+
+                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+                                "mmail send-tag Man10RealEstate ${Bukkit.getOfflinePlayer(uuid).name} &4&l[重要]土地の税金について 5 " +
+                                        "&6&l税額:$tax;" +
+                                        "&e&l土地ID:${rg.key};" +
+                                        "&6&e来月お支払いお願いします")
+                    }
+
+                }
 
                 "getbarrel" ->{
                     val barrel = ItemStack(Material.BARREL)
