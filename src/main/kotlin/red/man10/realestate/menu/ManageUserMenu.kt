bbe42684523e7c8fbf57c17eb7e3491838be9484
Jin Morikawa <forest611328@gmail.com>
2023-10-06 14:04:41 +0900

    メニュー追加

--- src/main/kotlin/red/man10/realestate/menu/ManageUserMenu.kt
+++ src/main/kotlin/red/man10/realestate/menu/ManageUserMenu.kt
@@ -1,4 +1,13 @@
 package red.man10.realestate.menu
 
-class ManageUserMenu {
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+import red.man10.realestate.region.User
+import red.man10.realestate.util.MenuFramework
+
+class ManageUserMenu(p:Player,val user:User) : MenuFramework(p, CHEST_SIZE,"${Bukkit.getOfflinePlayer(user.uuid).name}の管理"){
+
+    override fun init() {
+
+    }
 }
