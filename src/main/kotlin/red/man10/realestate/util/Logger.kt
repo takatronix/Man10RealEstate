ad7e366514da88f3621a41552f1fd18c076b57d2
Jin Morikawa <forest611328@gmail.com>
2024-01-28 20:39:32 +0900

    クエリ修正

--- src/main/kotlin/red/man10/realestate/util/Logger.kt
+++ src/main/kotlin/red/man10/realestate/util/Logger.kt
@@ -9,12 +9,12 @@ object Logger {
     fun logger(uuid:UUID,log:String,id: Int = -1){
         val p = Bukkit.getOfflinePlayer(uuid)
         MySQLManager.mysqlQueue.add("INSERT INTO log (date, player, uuid, region_id, log) " +
-                "VALUES (DEFAULT, '${p.name}', '${uuid}', $id '${log}')")
+                "VALUES (DEFAULT, '${p.name}', '${uuid}', $id, '${log}')")
     }
 
     fun logger(p:Player,log:String,id: Int = -1){
         MySQLManager.mysqlQueue.add("INSERT INTO log (date, player, uuid, region_id, log) " +
-                "VALUES (DEFAULT, '${p.name}', '${p.uniqueId}', $id '${log}')")
+                "VALUES (DEFAULT, '${p.name}', '${p.uniqueId}', $id, '${log}')")
     }
 
     fun logger(log: String ,id:Int = -1){
